b76fa4a2e006003587782f3ec461390b255c5d65
Arvind Panda <arvindpanda2007@gmail.com>
2026-05-12 13:43:57 +0530

    simple tokenizer implemented

--- tokenization/01_tokenizer.py
+++ tokenization/01_tokenizer.py
@@ -1,8 +1,15 @@
-import re 
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab           
+        self.int_to_str = {i:s for s,i in vocab.items()}
 
-with open("the-verdict.txt", "r", encoding="utf-8") as f:
-    raw_text = f.read()
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
 
-result = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
-result = [item.strip() for item in result if item.strip()]
-print(result)
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)   
+        return text    
